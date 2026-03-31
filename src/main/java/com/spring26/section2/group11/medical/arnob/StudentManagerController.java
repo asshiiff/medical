650f333ca9939bb6f338e88474ea71178582063f
DATTA <2221684@iub.edu.bd>
2026-04-01 00:00:30 +0600

    initial setup

---
+++ src/main/java/com/spring26/section2/group11/medical/arnob/StudentManagerController.java
@@ -0,0 +1,26 @@
+package com.spring26.section2.group11.medical.arnob;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+public class StudentManagerController {
+    @javafx.fxml.FXML
+    private Label display1;
+    @javafx.fxml.FXML
+    private ComboBox examSubjectCB;
+    @javafx.fxml.FXML
+    private Label display6;
+    @javafx.fxml.FXML
+    private Label display3;
+    @javafx.fxml.FXML
+    private Label display2;
+    @javafx.fxml.FXML
+    private Label display5;
+    @javafx.fxml.FXML
+    private Label display4;
+
+    @javafx.fxml.FXML
+    public void startExamButton(ActionEvent actionEvent) {
+    }
+}
