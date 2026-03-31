407f720210e3a7b1a4c31f90621253e7fec98d0c
asshiiff <ashifapple20@gmail.com>
2026-04-01 00:53:32 +0600

    create a analyzeQuestionDistribution.fxml

---
+++ src/main/java/com/spring26/section2/group11/medical/ashif/QuestionDistributionController.java
@@ -0,0 +1,19 @@
+package com.spring26.section2.group11.medical.ashif;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class QuestionDistributionController
+{
+    @javafx.fxml.FXML
+    private TableColumn typeCol;
+    @javafx.fxml.FXML
+    private TableView questionDistributionTable;
+    @javafx.fxml.FXML
+    private TableColumn eventCol;
+    @javafx.fxml.FXML
+    private TableColumn descriptionCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
