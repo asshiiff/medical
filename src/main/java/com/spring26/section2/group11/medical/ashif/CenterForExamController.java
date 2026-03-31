a5f7e50a1b6806ddfe0983fccf4668e623b32eff
asshiiff <ashifapple20@gmail.com>
2026-03-31 23:19:40 +0600

    create a set center for exam

---
+++ src/main/java/com/spring26/section2/group11/medical/ashif/CenterForExamController.java
@@ -0,0 +1,18 @@
+package com.spring26.section2.group11.medical.ashif;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+
+public class CenterForExamController
+{
+    @javafx.fxml.FXML
+    private ComboBox centerCB;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void centerButton(ActionEvent actionEvent) {
+    }
+}
