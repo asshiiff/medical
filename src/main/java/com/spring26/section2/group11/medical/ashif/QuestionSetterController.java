98f16982fb1126d6e9ea4f787501607107d4a500
asshiiff <ashifapple20@gmail.com>
2026-04-01 00:16:43 +0600

    create a question setter

---
+++ src/main/java/com/spring26/section2/group11/medical/ashif/QuestionSetterController.java
@@ -0,0 +1,25 @@
+package com.spring26.section2.group11.medical.ashif;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class QuestionSetterController
+{
+    @javafx.fxml.FXML
+    private TableColumn totalScoreCol;
+    @javafx.fxml.FXML
+    private TableColumn marksCol;
+    @javafx.fxml.FXML
+    private TableView quesstionSetterTable;
+    @javafx.fxml.FXML
+    private TableColumn rankCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void addButton(ActionEvent actionEvent) {
+    }
+}
