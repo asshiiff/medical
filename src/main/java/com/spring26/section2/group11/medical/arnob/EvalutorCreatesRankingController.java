30e0df52365f110193d3ce4f8744ca3ee1306473
DATTA <2221684@iub.edu.bd>
2026-04-01 00:27:35 +0600

    initial setup

---
+++ src/main/java/com/spring26/section2/group11/medical/arnob/EvalutorCreatesRankingController.java
@@ -0,0 +1,35 @@
+package com.spring26.section2.group11.medical.arnob;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class EvalutorCreatesRankingController {
+    @javafx.fxml.FXML
+    private TableColumn studentNameCol;
+    @javafx.fxml.FXML
+    private Label display1;
+    @javafx.fxml.FXML
+    private TableColumn marksCol;
+    @javafx.fxml.FXML
+    private TableColumn rankCol;
+    @javafx.fxml.FXML
+    private Label display2;
+    @javafx.fxml.FXML
+    private TableView rankTable;
+    @javafx.fxml.FXML
+    private TableColumn studentIDCol;
+
+    @javafx.fxml.FXML
+    public void saveRankingButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateRankingButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void exportPDFButton(ActionEvent actionEvent) {
+    }
+}
