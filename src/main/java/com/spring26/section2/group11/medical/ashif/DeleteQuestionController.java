568a680415d200b532999f4026bc4aa84cc83dce
asshiiff <ashifapple20@gmail.com>
2026-04-08 04:33:39 +0600

    zzz

---
+++ src/main/java/com/spring26/section2/group11/medical/ashif/DeleteQuestionController.java
@@ -0,0 +1,21 @@
+package com.spring26.section2.group11.medical.ashif;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class DeleteQuestionController
+{
+    @javafx.fxml.FXML
+    private TableView questionlistTable;
+    @javafx.fxml.FXML
+    private TableColumn questionList;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void deleteButton(ActionEvent actionEvent) {
+    }
+}
