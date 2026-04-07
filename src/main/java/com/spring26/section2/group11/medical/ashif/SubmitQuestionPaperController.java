641ca87e5897c149a73a429c2bbdc401a793b29a
asshiiff <ashifapple20@gmail.com>
2026-04-08 05:13:44 +0600

    zzz

---
+++ src/main/java/com/spring26/section2/group11/medical/ashif/SubmitQuestionPaperController.java
@@ -0,0 +1,24 @@
+package com.spring26.section2.group11.medical.ashif;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class SubmitQuestionPaperController
+{
+    @javafx.fxml.FXML
+    private Label display;
+    @javafx.fxml.FXML
+    private TableView questionPapersTable;
+    @javafx.fxml.FXML
+    private TableColumn questionPaperCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void submitButton(ActionEvent actionEvent) {
+    }
+}
