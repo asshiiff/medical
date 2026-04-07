f5adc629180f8229bbed7d183983b8457d2987d6
asshiiff <ashifapple20@gmail.com>
2026-04-08 03:31:41 +0600

    zzz

---
+++ src/main/java/com/spring26/section2/group11/medical/ashif/GenerateReportsController.java
@@ -0,0 +1,33 @@
+package com.spring26.section2.group11.medical.ashif;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class GenerateReportsController
+{
+    @javafx.fxml.FXML
+    private TableView previousDataTable;
+    @javafx.fxml.FXML
+    private Label display;
+    @javafx.fxml.FXML
+    private TableColumn previousDataCol;
+    @javafx.fxml.FXML
+    private ComboBox selectExamCB;
+    @javafx.fxml.FXML
+    private ComboBox reportTypeCB;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void downloadButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reportButton(ActionEvent actionEvent) {
+    }
+}
