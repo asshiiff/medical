2a06dde1069e5623adcbff04d083781b51632a3d
asshiiff <ashifapple20@gmail.com>
2026-04-08 04:13:25 +0600

    zzz

---
+++ src/main/java/com/spring26/section2/group11/medical/ashif/CancelResheduleExamController.java
@@ -0,0 +1,30 @@
+package com.spring26.section2.group11.medical.ashif;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class CancelResheduleExamController
+{
+    @javafx.fxml.FXML
+    private Label display;
+    @javafx.fxml.FXML
+    private DatePicker datePicker;
+    @javafx.fxml.FXML
+    private ComboBox newTimeCB;
+    @javafx.fxml.FXML
+    private TableView examlistTable;
+    @javafx.fxml.FXML
+    private TableColumn examlistCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void resheduleButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cancelButton(ActionEvent actionEvent) {
+    }
+}
