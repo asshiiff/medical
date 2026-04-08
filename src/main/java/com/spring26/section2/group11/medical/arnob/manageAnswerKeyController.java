e619169aee1f01d90859534aa5b8c0e090d89d54
DATTA <2221684@iub.edu.bd>
2026-04-08 22:26:16 +0600

    initial setup

---
+++ src/main/java/com/spring26/section2/group11/medical/arnob/manageAnswerKeyController.java
@@ -0,0 +1,25 @@
+package com.spring26.section2.group11.medical.arnob;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class manageAnswerKeyController {
+    @javafx.fxml.FXML
+    private TextField answerKeyField;
+    @javafx.fxml.FXML
+    private ComboBox courseCB;
+    @javafx.fxml.FXML
+    private ComboBox semesterCB;
+    @javafx.fxml.FXML
+    private Label display;
+    @javafx.fxml.FXML
+    private TableView answerTable;
+    @javafx.fxml.FXML
+    private TableColumn courseCol;
+    @javafx.fxml.FXML
+    private TableColumn answerKeyCol;
+
+    @javafx.fxml.FXML
+    public void saveButton(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/spring26/section2/group11/medical/arnob/studentPerformanceStatisticsController.java
@@ -0,0 +1,30 @@
+package com.spring26.section2.group11.medical.arnob;
+
+import javafx.event.ActionEvent;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
+public class studentPerformanceStatisticsController {
+    @javafx.fxml.FXML
+    private Label display1;
+    @javafx.fxml.FXML
+    private ComboBox subjectCB;
+    @javafx.fxml.FXML
+    private DatePicker toLocalDate;
+    @javafx.fxml.FXML
+    private DatePicker fromLocalDate;
+    @javafx.fxml.FXML
+    private Label display3;
+    @javafx.fxml.FXML
+    private Label display2;
+    @javafx.fxml.FXML
+    private PieChart resultChart;
+    @javafx.fxml.FXML
+    private Label display4;
+
+    @javafx.fxml.FXML
+    public void statisticsButton(ActionEvent actionEvent) {
+    }
+}
