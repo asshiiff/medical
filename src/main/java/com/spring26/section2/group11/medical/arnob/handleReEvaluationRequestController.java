ee58f64390aa7e186299f3d1b82382b19d9ad192
DATTA <2221684@iub.edu.bd>
2026-04-08 19:55:00 +0600

    initial setup

---
+++ src/main/java/com/spring26/section2/group11/medical/arnob/handleReEvaluationRequestController.java
@@ -0,0 +1,42 @@
+package com.spring26.section2.group11.medical.arnob;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class handleReEvaluationRequestController {
+    @javafx.fxml.FXML
+    private Label display1;
+    @javafx.fxml.FXML
+    private TextField studentIdField;
+    @javafx.fxml.FXML
+    private TextField requestMarksField;
+    @javafx.fxml.FXML
+    private TableColumn marksCol;
+    @javafx.fxml.FXML
+    private TableColumn statusCol;
+    @javafx.fxml.FXML
+    private TableColumn examCol;
+    @javafx.fxml.FXML
+    private TextField currentMarksField;
+    @javafx.fxml.FXML
+    private ComboBox statusCB;
+    @javafx.fxml.FXML
+    private TableColumn studentIdCol;
+    @javafx.fxml.FXML
+    private ComboBox selectExamCB;
+    @javafx.fxml.FXML
+    private TextField reasonField;
+    @javafx.fxml.FXML
+    private Label display2;
+
+    @javafx.fxml.FXML
+    public void updateButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void approveButton(ActionEvent actionEvent) {
+    }
+}
